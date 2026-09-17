8f934379b99b217a35684021857848a10dc1868a
LitePage Server <litepagesrb@gmail.com>
2026-09-17 13:45:23 +0000

    Site updated at 2026-09-17 13:45:23 UTC

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202609171312",
+        "/css/pixyll.css?202609171345",
         "/"
       ]);
     })
